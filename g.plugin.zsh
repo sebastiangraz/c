87287e5ec933b7d73c3de2afeea888ff4ddc341b
Sebastian Graz <sebastian@framer.com>
2020-02-14 14:24:17 +0100

    Create g.plugin.zsh

---
+++ g.plugin.zsh
@@ -0,0 +1,28 @@
+pull() {
+  if [ $# -eq 2 ]; then
+    git pull --rebase -q $2 "$(git rev-parse --abbrev-ref HEAD)"
+  else
+    git pull --rebase -q origin "$(git rev-parse --abbrev-ref HEAD)"
+  fi
+}
+
+push() {
+  if [ $# -eq 2 ]; then
+    git push -q $2 "$(git rev-parse --abbrev-ref HEAD)"
+  else
+    git push -q origin "$(git rev-parse --abbrev-ref HEAD)"
+  fi
+}
+
+g() {
+  git add "$(git rev-parse --show-toplevel)"
+  if [ $# -eq 1 ]; then
+    git commit -a -m "$1"
+  else
+    git commit -a -m "Commit"
+  fi
+  pull
+  push
+}
+
+
